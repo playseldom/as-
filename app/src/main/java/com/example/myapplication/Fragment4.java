d8849881138bdc51dfc3e4fb4953b07ccbab07f3
playseldom <2458702981@qq.com>
2023-10-15 21:35:22 +0800

    第一次进入git库，开始写作业

---
+++ app/src/main/java/com/example/myapplication/Fragment4.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Fragment4 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab4, container, false);
+        // 将一个xml压缩为一个视图
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/recycleactivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class recycleactivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List list;
+    Context context;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycleactivity);
+
+        recyclerView = findViewById(R.id.recyclerview);
+
+        list = new ArrayList();
+        for (int i = 0; i < 9; i++)  {
+          list.add("这是第"+i+"数据");
+        }
+
+        context = this;
+
+        //Myadapter adapter = new Myadapter();
+        //recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+    }
+}
