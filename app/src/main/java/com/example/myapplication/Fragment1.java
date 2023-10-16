ab4cb4fe5b006ef4b717ef1f6d089959b96e3f6c
playseldom <2458702981@qq.com>
2023-10-16 16:44:40 +0800

    最终版本，完成了列表

--- app/src/main/java/com/example/myapplication/Fragment1.java
+++ app/src/main/java/com/example/myapplication/Fragment1.java
@@ -1,21 +1,55 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
 public class Fragment1 extends Fragment {
+    RecyclerView recyclerView;
+    List list;
+    Myadapter adapter;
+    Context context;
+    View view;
+
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.tab1,container,false);
+        recyclerView=view.findViewById(R.id.recyclerview);//连接至tab1的控件
+        list=new ArrayList();
+        for(int i=0;i<9;i++){
+            list.add("这是第"+i+"行数据");
+        }
+
+        adapter=new Myadapter(view.getContext(),list);//把list导入
+        LinearLayoutManager manager=new LinearLayoutManager(view.getContext());
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(manager);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab1, container, false);
-        // 将一个xml压缩为一个视图
+        return view;
     }
+
 }
--- app/src/main/java/com/example/myapplication/Myadapter.java
+++ app/src/main/java/com/example/myapplication/Myadapter.java
@@ -1,7 +1,7 @@
 package com.example.myapplication;
 
-
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,41 +11,56 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
-public class Myadapter extends RecyclerView.Adapter<Myadapter.Myholder> {
+public class Myadapter extends RecyclerView.Adapter<Myadapter.Myholder> {//打小气泡 放行传入
 
+    //视图Holder指向行的指针
     Context context1;
-    List<String> list1;
+    List<String> list1;//没有给定节点类型
 
-    public Myadapter(Context context, List list){
-        context1 = context;
-        list1 = list;
+    //每一行一个textview与viewholder进行绑定
+    public Myadapter(Context context,List list){
+        context1=context;
+        list1=list;
+
     }
-
     @NonNull
     @Override
     public Myholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = new View(context1);
-        LayoutInflater.from(context1).inflate(R.layout.item,parent,false);
-        Myholder holder = new Myholder(view);
-        return holder;
+
+        View view=LayoutInflater.from(context1).inflate(R.layout.item,parent,false);
+        //压缩list
+        ;//布局的压缩
+//        Inflater inflater=new Inflater();
+//        inflater.inflate(); 真正的压缩
+        Myholder myholder=new Myholder(view);
+
+        return myholder;
     }
 
-    @Override
+    @Override//绑定 上数据
     public void onBindViewHolder(@NonNull Myholder holder, int position) {
-        holder.textView.setText(list1.get(position));
+
+        holder.textView.setText(list1.get(position));//用于设置TextView的文本内容为数据列表list1中指定位置的数据
+        // 将颜色设置为红色
+        holder.textView.setTextColor(Color.RED);
+
     }
 
-    @Override
+
+    @Override//行数
     public int getItemCount() {
         return list1.size();
     }
-
     public class Myholder extends RecyclerView.ViewHolder{
         TextView textView;
+
         public Myholder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.textView6);
+            textView=itemView.findViewById(R.id.textView11);//引用item.xml里id为textView1的textView作为Myholder这个类中
+            //textView对象的初始值
         }
     }
+
 }
